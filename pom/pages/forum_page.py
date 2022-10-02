43a05e5db18fba35d7f489225a65a85070c20acb
Sergey1991wolk <svwolk1991@gmail.com>
2022-10-02 23:38:28 +0300

    Initial commit

--- pom/pages/forum_page.py
+++ pom/pages/forum_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from pom.pages.locators import forum_page_locator as loc
 
 
-class LoginPage:
+class ForumPage:
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
@@ -16,3 +16,9 @@ class LoginPage:
     def find_elements(self, *args):
         by_name, by_val = args[0]
         return self.driver.find_element(by_name, by_val)
+
+    def private_project(self):
+        return self.find_element(loc.private_project)
+
+    def forum_without_login_error_message(self):
+        return self.find_element(loc.forum_without_login_error_message)

fb5bfb5a33ce5d35709b889e491fe554501ee7c4
Sergey1991wolk <svwolk1991@gmail.com>
2022-10-03 17:23:54 +0300

    Initial commit

---
+++ pom/tests/profile_page_test.py
@@ -0,0 +1,18 @@
+from pom.pages.profile_page import ProfilePage
+from pom.pages.login_page import LoginPage
+
+def test_sc_color_profile(driver):
+    login_page = LoginPage(driver)
+    login_page.open()
+    user_name_field = login_page.user_name_field()  #логинимся ввод пороля без remember me,
+    user_name_field.click()
+    user_name_field.send_keys('wf.anna@gmail.com')
+    password_field = login_page.user_password_field()
+    password_field.click()
+    password_field.send_keys('1Vk9rU&i@NtvD3pVf70y5UEu')
+    remember_me = login_page.remember_me_field()
+    remember_me.click()
+    login_page.login_button().submit()
+
+    light_color = ProfilePage(driver)
+    light_color.open()
